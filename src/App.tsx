f7ef73b0b2a32155ed0bd8f7cd37ce43e1088296
ivankupryianau <ivan.kupryianau@capital.com>
2023-10-12 19:45:54 +0200

    feat: restructure the state, add associativa array to the main object, adjust handlerFunctions

--- src/App.tsx
+++ src/App.tsx
@@ -11,45 +11,54 @@ type TodolistType = {
 }
 
 function App() {
-
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: false},
-        {id: v1(), title: "GraphQL", isDone: false},
-    ]);
-
-    function removeTask(id: string) {
-        let filteredTasks = tasks.filter(t => t.id !== id);
-        setTasks(filteredTasks);
+    function removeTask(id: string, todolistId: string) {
+        let todolistTasks = tasks[todolistId]
+        tasks[todolistId] = todolistTasks.filter(el => el.id !== id)
+        setTasks({...tasks});
     }
 
-    function addTask(title: string) {
+    function addTask(title: string, todolistId: string) {
         let task = {id: v1(), title: title, isDone: false};
-        let newTasks = [task, ...tasks];
-        setTasks(newTasks);
+        let todolistTasks = tasks[todolistId]
+        tasks[todolistId] = [task, ...todolistTasks]
+        setTasks({...tasks});
     }
 
-    function changeStatus(taskId: string, isDone: boolean) {
-        let task = tasks.find(t => t.id === taskId);
+    function changeStatus(taskId: string, isDone: boolean, todolistId: string) {
+        let todolistTasks = tasks[todolistId]
+        let task = todolistTasks.find(t => t.id === taskId);
+
         if (task) {
             task.isDone = isDone;
+            setTasks({...tasks});
         }
-
-        setTasks([...tasks]);
     }
 
-
-
+    let todoListId1 = v1();
+    let todoListId2 = v1();
 
 
     let [todoLists, setTodoLists] = useState<Array<TodolistType>>(
         [
-            {id: v1(), title: 'What to buy', filter: 'all'},
-            {id: v1(), title: 'What to learn', filter: 'all'},
+            {id: todoListId1, title: 'What to buy', filter: 'all'},
+            {id: todoListId2, title: 'What to learn', filter: 'all'},
         ]
     )
+
+    let [tasks, setTasks] = useState({
+        [todoListId1]: [
+            {id: v1(), title: 'What to buy', isDone: true},
+            {id: v1(), title: 'What to learn', isDone: false},
+        ],
+        [todoListId2]: [
+            {id: v1(), title: 'React', isDone: true},
+            {id: v1(), title: 'RestAPI', isDone: false},
+            {id: v1(), title: 'NoSQL', isDone: true},
+        ]
+    })
+
+
+
 
     return (
         <div className="App">
@@ -62,24 +71,24 @@ function App() {
                     }
                 }
 
-                let tasksForTodolist = tasks;
+                let tasksForTodolist = tasks[tl.id];
 
                 if (tl.filter === "active") {
-                    tasksForTodolist = tasks.filter(t => t.isDone === false);
+                    tasksForTodolist = tasks[tl.id].filter(t => !t.isDone);
                 }
                 if (tl.filter === "completed") {
-                    tasksForTodolist = tasks.filter(t => t.isDone === true);
+                    tasksForTodolist = tasks[tl.id].filter(t => t.isDone);
                 }
 
                 return <Todolist key={tl.id}
-                    id={tl.id}
-                    title={tl.title}
-                    tasks={tasksForTodolist}
-                    removeTask={removeTask}
-                    changeFilter={changeFilter}
-                    addTask={addTask}
-                    changeTaskStatus={changeStatus}
-                    filter={tl.filter}
+                                 id={tl.id}
+                                 title={tl.title}
+                                 tasks={tasksForTodolist}
+                                 removeTask={removeTask}
+                                 changeFilter={changeFilter}
+                                 addTask={addTask}
+                                 changeTaskStatus={changeStatus}
+                                 filter={tl.filter}
                 />
             })}
 
